df0f357937fe89626bc51c4f7e62d8813d5acf12
cokolele <lukasdz9457@gmail.com>
2018-01-29 21:13:54 +0100

    Added world tiles section parsing and Updated error handling

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-process.stdout.write('\033c'); //system("cls")
+process.stdout.write('\033c\n'); //system("cls")
 
 const TerrariaWorldParser = require("./terraria-world-parser.js");
-const world = new TerrariaWorldParser("./Canvas.wld");
 
-console.log(world.Load());
+try {
+	const world = new TerrariaWorldParser("./Canvas.wld");
+	const data = world.Load();
+	console.log(data)
+} catch(e) {
+	console.log(e.message);
+}
--- terraria-world-parser.js
+++ terraria-world-parser.js
@@ -1,15 +1,19 @@
-const fs = require("fs");
-
 class TerrariaUtilities {
 
 	constructor(path) {
-
+		const fs = require("fs");
 		this.buffer = fs.readFileSync( path , [null, "r+"]);
 		this.offset = 0;
+		this.world = {
+			pointers:[],
+			importants:[],
+			tiles: {
+				x:null,
+				y:null,
+				solids: [true,true,true,null,null,null,true,true,true,true,true,null,null,null,null,null,null,null,null,true,null,null,true,true,null,true,null,null,null,null,true,null,null,null,null,null,null,true,true,true,true,true,null,true,true,true,true,true,true,null,null,true,true,true,true,null,true,true,true,true,true,null,true,true,true,true,true,true,true,null,true,null,null,null,null,true,true,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,true,true,true,null,true,true,null,null,true,true,true,true,true,true,true,true,true,null,null,null,true,null,null,true,null,null,null,null,null,null,true,null,null,true,null,null,null,null,true,true,true,true,null,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,null,true,true,true,true,true,null,null,null,null,true,true,true,null,true,true,true,true,true,null,null,null,null,true,true,true,true,true,true,true,true,true,true,true,true,true,null,true,true,true,true,true,null,true,null,null,true,null,null,null,null,null,null,null,null,null,true,true,true,true,true,true,null,null,true,true,null,true,null,true,true,null,null,null,true,null,null,null,null,null,null,null,null,true,true,true,true,true,true,null,true,true,true,true,true,true,true,true,true,true,true,true,true,true,null,null,null,true,true,true,null,null,null,null,null,null,null,null,null,true,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,true,true,true,null,true,null,null,null,null,null,true,true,null,null,true,true,true,true,true,null,null,null,null,null,null,true,null,null,null,true,true,true,true,true,true,true,true,true,null,true,true,true,true,null,null,null,true,null,null,null,null,null,null,null,true,true,true,true,true,true,true,null,null,null,null,null,null,null,true,null,true,true,true,true,true,null,null,null,null,null,null,null,null,null,null,true,true,true,true,true,true,true,true,true,null,null,true,true,true,null,null,null,null,null,true,true,true,true,null,null,true,true,null,null,null,true,null,null,null,true,true,true,true,true,null,null,null,null,null,null,null,null,null,null,null,true,true,true,true,true,true,null,null,null,null,null,null,true,true,true],
+			},
+		}
 	}
-/**
- * Binary parsing methods
- */
 
 	ReadByte() {
 
@@ -22,6 +26,14 @@ class TerrariaUtilities {
 	ReadInt16() {
 
 		const data = this.buffer.readInt16LE( this.offset );
+		this.offset += 2;
+
+		return data;
+	}
+
+	ReadUInt16() {
+
+		const data = this.buffer.readUInt16LE( this.offset );
 		this.offset += 2;
 
 		return data;
@@ -43,7 +55,6 @@ class TerrariaUtilities {
 		return data;
 	}
       
-// www.github.com/deoxxa/dissolve
 	ReadInt64() {
 
 		const firstHalf = this.buffer.readInt32LE( this.offset );
@@ -118,52 +129,24 @@ class TerrariaUtilities {
 		this.offset += count;
 	}
 
+	SkipStrings(count = 1) {
+
+		for (let i = 0; i < count; i++) {
+
+			const stringLength = this.ReadByte();
+			this.offet += stringLength;
+		}
+	}
+
 	JumpTo(offset) {
 
 		this.offset = offset;
 	}
 
-/**
- * Binary data reading methods
- */
-
 	ReadGuid(bytesArray) {
 
 		this.SkipBytes(16);
 		return "TODO";
-	}
-
-	ReadValidatingMetaData() {
-
-		this.JumpTo(0);
-		this.version 		= this.ReadInt32();
-		this.magicNumber 	= this.ReadBytes(7).toString("ascii");
-		this.fileType 		= this.ReadByte();
-		
-		this.JumpTo(0x18);
-		this.pointers = [];
-		this.importants = [];
-
-		const numberOfPointers = this.ReadInt16();
-
-		for (let i = 0; i < numberOfPointers; i++) {
-			this.pointers[i] = this.ReadInt32();
-		}
-
-		const numberOfImportants = this.ReadInt16();
-		let num3 = 0;
-		let num4 = 128;
-
-		for (let i = 0; i < numberOfImportants; ++i) {
-
-			if (num4 == 128) {
-
-				num3 = this.ReadByte();
-				num4 = 1;
-			} else num4 = num4 << 1 ; 
-
-			if ((num3 & num4) == num4) this.importants[i] = true;
-		}
 	}
 }
 
@@ -174,51 +157,91 @@ class TerrariaWorldParser extends TerrariaUtilities {
 		try {
 			super(path);
 		} catch (e) {
-			throw new Error("Couldn't read world file: " + e.message);
-		}
 
-		try {
-			this.ReadValidatingMetaData();
-		} catch (e) {
-			throw new Error("Couldn't read metadata: Corrupted world file.");
-		}
-
-		if ( this.version < 194 || this.magicNumber != "relogic" || this.fileType != 2 || this.pointers.length != 10 || this.importants.length != 470 ) {
-			throw new Error("Invalid metadata: World file version is not supported (only 1.3.5.8) or Corrupted world file");
+			if (e.code = "ENOENT") 
+				e.message = `terraria-world-parser error \ndescription: wrong path \ndetails: ${e.stack}`;
+			else 
+				e.message = `terraria-world-parser error \ndetails: ${e.stack}`;
+			throw e;
 		}
 	}
 
 	Load() {
 
 		let data = {};
+		
+		try {
 
-//		data.fileFormatHeader 	= this.LoadFileFormatHeader();
-//		data.header 			= this.LoadHeader();
-		data.worldTiles 		= this.LoadWorldTiles();
+			data.fileFormatHeader 	= this.LoadFileFormatHeader();
+			data.header 			= this.LoadHeader();
+			data.worldTiles 		= this.LoadWorldTiles();
+			
+			delete data.fileFormatHeader;
+			delete data.header;
+			delete data.worldTiles;
 
+		} catch (e) {
+
+			if (e.stack.includes("LoadFileFormatHeader")) 
+				e.message = `terraria-world-parser error \ndescription: potentionally corrupted file format header section \ndetails: ${e.stack}`;
+			else if (e.stack.includes("LoadHeader")) 
+				e.message = `terraria-world-parser error \ndescription: potentionally corrupted header section \ndetails: ${e.stack}`;
+			else if (e.stack.includes("LoadWorldTiles")) 
+				e.message = `terraria-world-parser error \ndescription: potentionally corrupted world tiles section \ndetails: ${e.stack}`;
+			else 
+				e.message = `terraria-world-parser error \ndetails: ${e.stack}`;
+			throw e;
+		}
+		
 		return data;
 	}
 
 	LoadFileFormatHeader() {
-		
-		let data = {};
-		this.JumpTo(0xC);
-		
-		data.version 		= this.version;
-		data.magicNumber 	= this.magicNumber;
-		data.fileType 		= this.fileType;
-		data.revision 		= this.ReadUInt32();
-		data.favorite  		= "0 (always zero)";
-		data.pointers 		= this.pointers;
-		data.importants 	= this.importants;
 
+		let data = {};
+
+		data.version 		= this.ReadInt32();
+		data.magicNumber 	= this.ReadBytes(7).toString("ascii");
+		data.fileType 		= this.ReadByte();
+		data.revision 		= this.ReadUInt32();
+		data.favorite  		= "0 (always zero)"; this.SkipBytes(8);
+		data.pointers 		= [];
+		data.importants 	= [];
+
+		const cPointers = this.ReadInt16();
+
+		for (let i = 0; i < cPointers; i++) {
+			data.pointers[i] = this.ReadInt32();
+		}
+
+		const cImportants = this.ReadInt16();
+		let num3 = 0;
+		let num4 = 128;
+
+		for (let i = 0; i < cImportants; ++i) {
+
+			if (num4 == 128) {
+
+				num3 = this.ReadByte();
+				num4 = 1;
+			} else num4 = num4 << 1 ; 
+
+			if ((num3 & num4) == num4) data.importants[i] = true;
+		}
+
+		if ( data.version < 194 || data.magicNumber != "relogic" || data.fileType != 2 || data.pointers.length != 10 || data.importants.length != 470 )
+			throw new Error("world file version is not supported (only 1.3.5.8) or corrupted metadata");
+		if ( this.offset != data.pointers[0] ) 
+			throw new Error("file format header section position did not end where it should");
+
+		this.world.pointers = data.pointers;
+		this.world.importants = data.importants;
 		return data;
 	}
 
 	LoadHeader() {
 
 		let data = {};
-		this.JumpTo(this.pointers[0]);
 
 		data.mapName 				= this.ReadString();
 		data.seedText 				= this.ReadString();
@@ -318,7 +341,7 @@ class TerrariaWorldParser extends TerrariaUtilities {
 		data.setBG7 				= this.ReadByte();
 		data.cloudBGActive 			= this.ReadInt32();
 		data.cloudBGAlpha 			= data.cloudBGActive < 1 ? 0 : 1;
-		data.cloudBGActive 			= "TODO" //(float) -data.genRand.Next(8640, 86400);
+		data.cloudBGActive 			= Math.random() * (86400 - 8640) + 8640; //(float) -data.genRand.Next(8640, 86400);
 		data.numClouds 				= this.ReadInt16();
 		data.windSpeedSet 			= this.ReadSingle();
 		data.windSpeed 				= data.windSpeedSet;
@@ -377,22 +400,153 @@ class TerrariaWorldParser extends TerrariaUtilities {
 		data.Temp_Sandstorm_TimeLeft 		= this.ReadInt32();
 		data.Temp_Sandstorm_Severity 		= this.ReadSingle();
 		data.Temp_Sandstorm_IntendedSeverity = this.ReadSingle();
-		data.position = this.offset;
+		data.savedBartender 				= this.ReadBoolean();
+		data.DD2Event_DownedInvasionT1 		= this.ReadBoolean();
+		data.DD2Event_DownedInvasionT2 		= this.ReadBoolean();
+		data.DD2Event_DownedInvasionT3 		= this.ReadBoolean();
 
-		data.savedBartender 			= this.ReadBoolean();
-        data.DD2Event_DownedInvasionT1 	= this.ReadBoolean();
-        data.DD2Event_DownedInvasionT2 	= this.ReadBoolean();
-        data.DD2Event_DownedInvasionT3 	= this.ReadBoolean();
+		if (this.offset != this.world.pointers[1])
+			throw new Error("header section position did not end where it should");
 
+		this.world.tiles.x = data.maxTilesX;
+		this.world.tiles.y = data.maxTilesY;
 		return data;
 	}
 
 	LoadWorldTiles() {
 
-		let data = {};
-		this.JumpTo(this.pointers[1]);
+		let data;
+		this.JumpTo(this.world.pointers[1]);
 
+		data = new Array(this.world.tiles.x);
+		for (let x = 0; x < this.world.tiles.x; x++) {
+
+			data[x] = new Array(this.world.tiles.y);
+			for (let y = 0; y < this.world.tiles.y; y++) {
+
+				const tile = this.DeserializeTileData();
+
+				data[x][y] = tile;
+
+				let RLE = tile.RLE;
+				delete tile.RLE;
+
+				while(RLE > 0) {
+					y++;
+					RLE--;
+					data[x][y] = tile;
+				}
+			}
+		}
+
+		if (this.offset != this.world.pointers[2])
+			throw new Error("world tiles section position did not end where it should");
+		
 		return data;
+	}
+
+	DeserializeTileData() {
+
+		let tile = {};
+
+		tile.blockId = -1;
+		let flags2, flags3;
+		const flags1 = this.ReadByte();
+
+	// flags2 present
+		if ((flags1 & 1) == 1) {
+			flags2 = this.ReadByte();
+
+		// flags3 present
+			if ((flags2 & 1) == 1)
+				flags3 = this.ReadByte();
+		}
+
+	// contains block
+		if ((flags1 & 2) == 2) {
+
+		// block id has 1 byte / 2 bytes
+			if ((flags1 & 32) == 32) tile.blockId = this.ReadUInt16();
+			else tile.blockId = this.ReadByte();
+
+		// important tile (animated, big sprite, more variants...)
+			if (this.world.importants[tile.blockId]) {
+				tile.frameX = this.ReadInt16();
+				tile.frameY = tile.blockId == 144 ? 0 : this.ReadInt16();
+			} else {
+				tile.frameX = -1;
+				tile.frameY = -1;
+			}
+
+		// painted block
+			if ((flags3 & 8) == 8) {
+				if (!tile.color) tile.color = {};
+				tile.color.block = this.ReadByte();
+			}
+		}
+
+	// contains wall
+		if ((flags1 & 4) == 4) {
+			tile.wall = this.ReadByte();
+
+		// painted wall
+			if ((flags3 & 16) == 16) {
+				if (!tile.color) tile.color = {};
+				tile.color.wall = this.ReadByte();
+			}
+		}
+
+	// liquid informations
+		const liquidType = (flags1 & 24) >> 3;
+		if (liquidType != 0) {
+			
+			if (!tile.liquid) tile.liquid = {};
+			tile.liquid.amount = this.ReadByte();
+			switch(liquidType) {
+				case 1: tile.liquid.type = "water"; break;
+				case 2: tile.liquid.type = "lava"; break;
+				case 3: tile.liquid.type = "honey"; break;
+			}
+		}
+
+	// flags2 has any other informations than flags3 presence
+		if (flags2 > 1) {
+			
+			if (!tile.wiring) tile.wiring = {};
+			if (!tile.wiring.wires) tile.wiring.wires = {};
+			if ((flags2 & 2) == 2) tile.wiring.wires.red = true;
+			if ((flags2 & 4) == 4) tile.wiring.wires.blue = true;
+			if ((flags2 & 8) == 8) tile.wiring.wires.green = true;
+
+			const hammeredBlock = (flags2 & 112) >> 4;
+			if (hammeredBlock != 0 && this.world.tiles.solids[tile.blockId]) {
+
+				switch(hammeredBlock) {
+					case 1: tile.hammered = "half"; break;
+					case 2: tile.hammered = "TR"; break;
+					case 3: tile.hammered = "TL"; break;
+					case 4: tile.hammered = "BR"; break;
+					case 5: tile.hammered = "BL"; break;
+				}
+			}
+		}
+
+	// flags3 has any informations
+		if (flags3 > 0) {
+			
+			if (!tile.wiring) tile.wiring = {};
+			if ((flags3 & 2) == 2) tile.wiring.hasActuator = true;
+			if ((flags3 & 4) == 4) tile.wiring.actuated = true;
+			if ((flags3 & 32) == 32) tile.wiring.wires.yellow = true;
+		}
+
+		switch ((flags1 & 192) >> 6) {
+			case 0: tile.RLE = 0; break;
+			case 1: tile.RLE = this.ReadByte(); break;
+			default: tile.RLE = this.ReadInt16(); break;
+		}
+
+		return tile;
 	}
 
 }
