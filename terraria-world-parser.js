56d690e6774f5fd902589138e909a76bb5a27532
cokolele <lukasdz9457@gmail.com>
2018-01-31 19:37:36 +0100

    Added NPCs section parsing

--- terraria-world-parser.js
+++ terraria-world-parser.js
@@ -12,6 +12,36 @@ class TerrariaUtilities {
 				y:null,
 				solids: [true,true,true,null,null,null,true,true,true,true,true,null,null,null,null,null,null,null,null,true,null,null,true,true,null,true,null,null,null,null,true,null,null,null,null,null,null,true,true,true,true,true,null,true,true,true,true,true,true,null,null,true,true,true,true,null,true,true,true,true,true,null,true,true,true,true,true,true,true,null,true,null,null,null,null,true,true,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,true,true,true,null,true,true,null,null,true,true,true,true,true,true,true,true,true,null,null,null,true,null,null,true,null,null,null,null,null,null,true,null,null,true,null,null,null,null,true,true,true,true,null,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,null,true,true,true,true,true,null,null,null,null,true,true,true,null,true,true,true,true,true,null,null,null,null,true,true,true,true,true,true,true,true,true,true,true,true,true,null,true,true,true,true,true,null,true,null,null,true,null,null,null,null,null,null,null,null,null,true,true,true,true,true,true,null,null,true,true,null,true,null,true,true,null,null,null,true,null,null,null,null,null,null,null,null,true,true,true,true,true,true,null,true,true,true,true,true,true,true,true,true,true,true,true,true,true,null,null,null,true,true,true,null,null,null,null,null,null,null,null,null,true,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,true,true,true,null,true,null,null,null,null,null,true,true,null,null,true,true,true,true,true,null,null,null,null,null,null,true,null,null,null,true,true,true,true,true,true,true,true,true,null,true,true,true,true,null,null,null,true,null,null,null,null,null,null,null,true,true,true,true,true,true,true,null,null,null,null,null,null,null,true,null,true,true,true,true,true,null,null,null,null,null,null,null,null,null,null,true,true,true,true,true,true,true,true,true,null,null,true,true,true,null,null,null,null,null,true,true,true,true,null,null,true,true,null,null,null,true,null,null,null,true,true,true,true,true,null,null,null,null,null,null,null,null,null,null,null,true,true,true,true,true,true,null,null,null,null,null,null,true,true,true],
 			},
+			npcTypes: {
+				"17": "Merchant",
+				"18": "Nurse",
+				"19": "Arms Dealer",
+				"20": "Dryad",
+				"22": "Guide",
+				"37": "Old Man",
+				"38": "Demolitionist",
+				"54": "Clothier",
+				"107": "Goblin Tinkerer",
+				"108": "Wizard",
+				"124": "Mechanic",
+				"142": "Santa Claus",
+				"160": "Truffle",
+				"178": "Steampunker",
+				"207": "Dye Trader",
+				"208": "Party Girl",
+				"209": "Cyborg",
+				"227": "Painter",
+				"228": "Witch Doctor",
+				"229": "Pirate",
+				"353": "Stylist",
+				"369": "Angler",
+				"422": "Vortex Pillar",
+				"441": "Tax Collector",
+				"493": "Stardust Pillar",
+				"507": "Nebula Pillar",
+				"517": "Solar Pillar",
+				"550": "Tavernkeep",
+			}
 		}
 	}
 
@@ -97,10 +127,20 @@ class TerrariaUtilities {
 	}
 
 	ReadDouble() {
+
 		const data = this.buffer.readDoubleLE( this.offset );
 		this.offset += 8;
 
 		return data;
+	}
+
+	ReadVector2() {
+
+		let data = {}
+		data.x = this.ReadSingle();
+		data.y = this.ReadSingle();
+
+		return data
 	}
 
 	ReadBoolean() {
@@ -177,7 +217,8 @@ class TerrariaWorldParser extends TerrariaUtilities {
 			//data.header           = this.LoadHeader();
 			//data.worldTiles       = this.LoadWorldTiles();
 			//data.chestsData       = this.LoadChests();
-			data.signsData          = this.LoadSigns();
+			//data.signsData        = this.LoadSigns();
+			//data.npcsData 		= this.LoadNPCs();
 
 		} catch (e) {
 
@@ -187,6 +228,12 @@ class TerrariaWorldParser extends TerrariaUtilities {
 				e.message = `terraria-world-parser error \ndescription: potentionally corrupted header section \ndetails: ${e.stack}`;
 			else if (e.stack.includes("LoadWorldTiles")) 
 				e.message = `terraria-world-parser error \ndescription: potentionally corrupted world tiles section \ndetails: ${e.stack}`;
+			else if (e.stack.includes("LoadChests")) 
+				e.message = `terraria-world-parser error \ndescription: potentionally corrupted chests section \ndetails: ${e.stack}`;
+			else if (e.stack.includes("LoadSigns")) 
+				e.message = `terraria-world-parser error \ndescription: potentionally corrupted signs section \ndetails: ${e.stack}`;
+			else if (e.stack.includes("LoadNPCs")) 
+				e.message = `terraria-world-parser error \ndescription: potentionally corrupted NPCs section \ndetails: ${e.stack}`;
 			else 
 				e.message = `terraria-world-parser error \ndetails: ${e.stack}`;
 			throw e;
@@ -605,6 +652,49 @@ class TerrariaWorldParser extends TerrariaUtilities {
 		}
 
 		return data;
+	}
+
+	LoadNPCs() {
+
+		let data = {};
+		this.JumpTo(this.world.pointers[4]);
+
+		if (this.ReadBoolean()) {
+
+			//"reversing" previous read so the next loop does not ignore first npc's data
+			this.SkipBytes(-1);
+			data.npcsCount = 1;
+			data.npcs = [];
+		} else
+			data.npcsCount = 0;
+		
+		let i;
+		for (i = 0; this.ReadBoolean(); i++) {
+
+			data.npcs[i] = {};
+			data.npcs[i].id 		= this.ReadInt32();
+			data.npcs[i].npc 		= this.world.npcTypes[ data.npcs[i].id ];
+			if (!data.npcs[i].npc) delete data.npcs[i].npc;
+
+			data.npcs[i].name 		= this.ReadString();
+			data.npcs[i].x 			= this.ReadSingle();
+			data.npcs[i].y 			= this.ReadSingle();
+			data.npcs[i].homeless 	= this.ReadBoolean();
+			data.npcs[i].homeTileX 	= this.ReadInt32();
+			data.npcs[i].homeTileY 	= this.ReadInt32();
+		}
+
+		for (; this.ReadBoolean(); i++) {
+			
+			if (!data.npcs) data.npcs = [];
+			data.npcs[i] = {};
+			data.npcs[i].id 	= this.ReadInt32();
+			data.npcs[i].npc 	= this.world.npcTypes[ data.npcs[i].id ];
+
+			data.npcs[i].position = this.ReadVector2();
+		}
+
+		return data.npcs[1];
 	}
 }
 
